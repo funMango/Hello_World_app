cf9243ffce305c95b641f71c5590087402619fda
funMango <bluemango608@gmail.com>
2023-07-10 23:54:58 +0900

    DatePicker(function): current time

--- DatePicker/DatePicker/ViewController.swift
+++ DatePicker/DatePicker/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let inteval = 1.0
+    var count = 0
+    
     @IBOutlet var IblCurrentTime: UILabel!
     @IBOutlet var IblPickerTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Timer.scheduledTimer(timeInterval: inteval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
     
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
@@ -21,5 +27,15 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
         IblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        IblCurrentTime.text = String(count)
+//        count += 1
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd EEE HH:mm:ss"
+        IblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
     }
 }
