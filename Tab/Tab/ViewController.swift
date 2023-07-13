7e148d0b727ddc905e46282f0e0cc53df5736354
funMango <bluemango608@gmail.com>
2023-07-13 20:06:45 +0900

    Tab(layout): input tabs

---
+++ Tab/Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by 이민호 on 2023/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
