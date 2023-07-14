294f3b18411e854ec52a6ef2877336bb9132a757
funMango <bluemango608@gmail.com>
2023-07-14 21:06:19 +0900

    Navigation(function): switch on off

--- Navigation/Navigation/EditViewController.swift
+++ Navigation/Navigation/EditViewController.swift
@@ -9,26 +9,41 @@ import UIKit
 
 protocol EditDelegate {
     func didMessageEditDone(_ controller:EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
 }
 
 class EditViewController: UIViewController {
     var textWayValue: String = ""
     var textMessage: String = ""
     var delegate : EditDelegate?
+    var isOn = false
     
     @IBOutlet var lblWay: UILabel!
     @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swisOn: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lblWay.text = textWayValue
         txMessage.text = textMessage
+        swisOn.isOn = isOn
+    }
+    
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
         if delegate != nil {
             delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
         }
         _ = navigationController?.popViewController(animated: true)
     }
+            
+    
 }
--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 class ViewController: UIViewController, EditDelegate {
         
     @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    var isOn = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgView.image = imgOn
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -24,10 +31,21 @@ class ViewController: UIViewController, EditDelegate {
             editViewController.textWayValue = "segue : use Bar button"
         }
         editViewController.textMessage = txMessage.text!
+        editViewController.isOn = isOn
         editViewController.delegate = self
     }
     
     func didMessageEditDone(_ controller: EditViewController, message: String) {
         txMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
     }
 }
