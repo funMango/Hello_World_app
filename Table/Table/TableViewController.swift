552a9f0aaa0a4c805dd417af2661147b77f8a725
funMango <bluemango608@gmail.com>
2023-07-17 16:01:15 +0900

    Table(function): delete table row

--- Table/Table/TableViewController.swift
+++ Table/Table/TableViewController.swift
@@ -45,6 +45,16 @@ class TableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: (indexPath as NSIndexPath).row)
+            itemsImageFile.remove(at: (indexPath as NSIndexPath).row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+             //Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -54,17 +64,9 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    
+    
+    
 
     /*
     // Override to support rearranging the table view.
