ae008dceb0489e22441944edbff8ef0abcf3397d
funMango <bluemango608@gmail.com>
2023-07-18 12:16:41 +0900

    Audio(layout): layout clear

---
+++ AutoLayout2/AutoLayout2/MySwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  MySwiftUIView.swift
+//  AutoLayout2
+//
+//  Created by 이민호 on 2023/07/17.
+//
+
+import SwiftUI
+
+struct MySwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MySwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MySwiftUIView()
+    }
+}
---
+++ AutoLayout2/AutoLayout2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  AutoLayout2
+//
+//  Created by 이민호 on 2023/07/17.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+struct Content: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Content_Previews: PreviewProvider {
+    static var previews: some View {
+        Content()
+    }
+}
